7137a7c29b89f98ca99af08d8118add05b7cfa17
mayda <mayda.werfali@gmail.com>
2021-10-23 12:56:29 +0200

    added master layout files

---
+++ resources/views/admin/layouts/sidebar_menu.blade.php
@@ -0,0 +1,21 @@
+<div class="left_col scroll-view">
+    <div class="navbar nav_title border-0">
+        <a href="/" class="site_title"><i
+                class="admin_logo"></i><span>ADMIN</span></a>
+    </div>
+    <div class="clearfix"></div>
+    <!-- sidebar menu -->
+
+    <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+        <div class="menu_section">
+            <ul class="nav side-menu">
+                <li><a href="/"><i class="fa fa-home"></i> Dashboard</a></li>
+
+            </ul>
+
+        </div>
+
+    </div>
+    <!-- /sidebar menu -->
+
+</div>
---
+++ resources/views/admin/layouts/top_nav.blade.php
@@ -0,0 +1,23 @@
+<div class="nav_menu">
+    <div class="nav toggle">
+        <a id="menu_toggle"><i class="fa fa-bars"></i></a>
+    </div>
+
+    <nav class="nav navbar-nav">
+        <ul class=" navbar-right">
+            <li class="nav-item dropdown open">
+                <a href="javascript:;" class="user-profile dropdown-toggle" aria-haspopup="true" id="navbarDropdown" data-toggle="dropdown" aria-expanded="false">
+                    {{Auth::user()}}
+                </a>
+                <div class="dropdown-menu dropdown-usermenu pull-right" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="/" target="_blank"><i class="fa fa-home pull-right"></i> Home Page</a>
+                    <a class="dropdown-item" href="/" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><i class="fa fa-sign-out pull-right"></i> Logout</a>
+                    <form id="logout-form" action="/" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                </div>
+            </li>
+        </ul>
+    </nav>
+
+</div>
